22364eda5b9ea5cb1a455069d5c7b1a5098ed1f0
arun02akashr <arun_akashr@tamu.edu>
2025-02-07 23:28:23 -0600

    User login and Signup

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+
+    # Add bcrypt for secure password storage
+    has_secure_password
+    
+    # Validations
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  
+    # Validate password presence, length, and complexity
+    validates :password, presence: true, length: { minimum: 8 }, format: { 
+      with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+\z/, 
+      message: "must include at least one uppercase letter, one lowercase letter, one number, and one special character" 
+    }    
+  end
