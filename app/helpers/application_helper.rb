eba3a0e772c4aa91cc615769bdd74df1b7bafda5
arun02akashr <arun_akashr@tamu.edu>
2025-02-08 10:31:46 -0600

    User Login Signup_2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def generate_message(user, attribute)
+        case attribute
+        when :password_confirmation
+          "Passwords don't match"
+        else
+          @user.errors.full_messages_for(attribute).join(", ")
+        end
+    end
 end
