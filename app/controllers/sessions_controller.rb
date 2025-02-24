e7c4da671b58f4b26f9dda433e1a17659eee5ce6
root <root@LAPTOP-5FOB9NO7.>
2025-02-24 02:05:12 -0600

    Implemented user session tracking: login/logout times are recorded

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,10 @@ class SessionsController < Devise::SessionsController
     if user && user.valid_password?(params[:user][:password]) # Devise method to check password
       sign_in(:user, user) # ✅ Fix: Specify the scope
       #sign_in(user)
+
+      # ✅ Track Login Event
+      Session.create(user: user, login_time: Time.current)
+
       # Retrieve the stored location BEFORE redirecting
       redirect_to after_sign_in_path_for(user), notice: "Signed in successfully!"
     else
@@ -23,9 +27,15 @@ class SessionsController < Devise::SessionsController
   end
 
   def destroy
+    if current_user
+    # ✅ Find last session & update logout time
+    user_session = Session.where(user_id: current_user.id).last
+    user_session.update(logout_time: Time.current) if user_session.present?
+
     sign_out(current_user)# Devise method to log out
     reset_session  # ✅ Clears all session data (including stored locations)
     redirect_to new_user_session_path, notice: "Logged out successfully"
+    end
   end
 
   private
