1f5611268bbef4a47a8c28ab70e172ce1ab7767f
Rafael <rafaeldms27@icloud.com>
2025-03-21 09:46:57 -0500

    Add sprint retrospective document

--- spec/system/profile_dropdown_spec.rb
+++ spec/system/profile_dropdown_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe "Profile Dropdown", type: :system do
     login_as(user, scope: :user)
     visit dashboard_path
     expect(page).to have_css('.navbar', wait: 5)
+    puts "✅ Test Passed: showing user on nav-tab"
   end
 
   it "shows user information when clicking profile icon" do
@@ -33,6 +34,7 @@ RSpec.describe "Profile Dropdown", type: :system do
       expect(page).to have_content(user.email)
       expect(page).to have_content(Session.where(user: user).last.login_time.strftime("%Y-%m-%d %H:%M"))
       expect(page).to have_button("Logout")
+      puts "✅ Test Passed: View Showing Logout button"
     end
   end
 
@@ -43,10 +45,12 @@ RSpec.describe "Profile Dropdown", type: :system do
     
     page.find("body").click
     expect(page).not_to have_css("#profile-dropdown-content.active")
+    puts "✅ Test Passed: Close when click outside"
   end
 
   it "logs out user when clicking logout button" do
     expect(page).to have_css("#profile-trigger", wait: 5)
+    puts "✅ Test Passed: log out when user click logout"
     find("#profile-trigger").click
     
     within("#profile-dropdown-content") do
@@ -58,5 +62,6 @@ RSpec.describe "Profile Dropdown", type: :system do
 
     expect(page).to have_current_path(new_user_session_path)
     expect(page).to have_content("Logged out successfully")
+    puts "✅ Test Passed: redirtect to user login"
   end
 end
